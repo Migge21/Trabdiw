ef9f6555b50a47a0dcd0d86f8df89f2136a27459
Migge21 <22001964@aluno.cotemig.com.br>
2023-05-30 21:34:03 -0300

    Funciona o detalhes e a pag inicial

--- js/detalhes.js
+++ js/detalhes.js
@@ -1,92 +1,64 @@
 function detalharProdutos(idProduto) {
-  console.log(idProduto);
-  let requisicao =
-    "https://fakestoreapi.com/products/category/electronics?id=" + idProduto;
-  fetch(requisicao)
-    .then((res) => res.json())
-    .then((produto) => mostrar(produto));
-}
-
-function mostrar(produtosAtual) {
-  var conteudoPrincipal = document.getElementById("Principal");
-
-  var cc = produtosAtual
-    .map(function (produto) {
-      return `<div class="CardContainer">
-      <div class="CardConteudo">
-        <img
-          class="mx-auto"
-          src="${produto.image}"
-          alt=""
-          height="100px"
-        />
-        <h3 class="CardTitulo">${produto.title}</h3>
-        
-        <div class="small">
-          <span class="fa fa-star checked"></span>
-          (${produto.rating.rate})
-        </div>
-        <div class="d-flex flex-column">
-          <b>
-            <large>$${produto.price}</large>
-          </b>
-          <a class="btn btn-primary" href="detalhes.html?id=${produto.id}" onclick="exibirDetalhes(${produto.id})">Detalhes</a>
-        </div>
-      </div>
-    </div>
-     `;
-    })
-    .join("");
-
-  conteudoPrincipal.innerHTML += cc;
-}
-
-function exibirDetalhes(idProduto) {
-  console.log(idProduto);
   let requisicao = "https://fakestoreapi.com/products/" + idProduto;
   fetch(requisicao)
     .then((res) => res.json())
-    .then((produto) => {
-      var detalhesProduto = document.getElementById("detalhesProduto");
-      var html = `
-      <div class="d-flex flex-row">
-      <div class="DetalhesContainer">
-        <div class="DetalhesConteudo">
-          <img
-            class="mx-auto"
-            src="${produto.image}"
-            alt=""
-            height="200px"
-            width="fit-content"
-          />
-          <div>
-            <h3 class="DetalhesTitulo">${produto.title}</h3>
 
-            <div class="small">
-              <span class="fa fa-star checked">Avaliações:</span>
-              (${produto.rating.rate})
+    .then((produto) => {
+      console.log(produto);
+      exibirDetalhes(produto);
+    });
+}
+
+function exibirDetalhes(produto) {
+  console.log(produto);
+
+  var detalhesProduto = document.getElementById("detalhesProduto");
+
+  var html = `
+        <div class="d-flex flex-row">
+        <div class="DetalhesContainer">
+          <div class="DetalhesConteudo">
+          <div class=d-flex>
+          <div>
+            <img
+              class="mx-auto"
+              src="${produto.image}"
+              alt=""
+              width="300px"
+              height="300px"
+              width="fit-content"
+            />
             </div>
-            <div class="d-flex flex-column">
-              <b>
-                <large class="preco">$${produto.price}</large>
-              </b>
-              <h4>${produto.description}</h4>
+            <div class="p-3">
+              <h3 class="DetalhesTitulo">${produto.title}</h3>        
+              <h5 class="green">Avaliações:</h5>    
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                (${produto.rating.rate})
+              
+              <div class="d-flex flex-column">
+                <b>
+                  <large class="preco">$${produto.price}</large>
+                </b>
+                <h4>${produto.description}</h4>
+              </div>
+              <a class="btn btn-primary" href="index.html">Voltar</a>
             </div>
-            <a class="btn btn-primary">Detalhes</a>
+          </div>
           </div>
         </div>
       </div>
-    </div>
-    `;
-      console.log(html);
-      detalhesProduto.innerHTML = html;
-    });
+      `;
+  console.log(html);
+  detalhesProduto.innerHTML = html;
 }
 
 window.onload = () => {
   let idParametro = new URLSearchParams(window.location.search);
   let identificador = idParametro.get("id");
-
+  console.log(identificador);
   detalharProdutos(identificador);
-  exibirDetalhes(identificador);
 };
---
+++ js/filtro.js
@@ -0,0 +1,39 @@
+function mostrar(arroz) {
+  var conteudoPrincipal = document.getElementById("Principal");
+
+  var cc = arroz
+    .map(function (produto) {
+      return `<div class="CardContainer">
+        <div class="CardConteudo">
+          <img
+            src="${produto.image}"
+            alt=""
+            height="150px"
+          />
+          <h4 class="CardTitulo">${produto.title}</h4>
+          
+          <div class="small">
+            <span class="fa fa-star checked"></span>
+            (${produto.rating.rate})
+          </div>
+          <div class="d-flex flex-column">
+            <b>
+              <p>$${produto.price}</p>
+            </b>
+            <a class="btn btn-primary" href="detalhes.html?id=${produto.id}"">Detalhes</a>
+          </div>
+        </div>
+      </div>
+       `;
+    })
+    .join("");
+
+  conteudoPrincipal.innerHTML += cc;
+}
+function buscarNoServidor() {
+  fetch("https://fakestoreapi.com/products/category/electronics")
+    .then((resposta) => resposta.json())
+    .then((dados) => mostrar(dados));
+}
+
+window.onload = buscarNoServidor();
